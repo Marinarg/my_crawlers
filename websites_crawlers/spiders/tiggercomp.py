0b34654fa129afd6f717fd94aba2f436fbbcb644
Marinarg <marinarg_09@hotmail.com>
2021-11-16 19:31:23 -0300

    fix tiggercomp crawler

--- websites_crawlers/spiders/tiggercomp.py
+++ websites_crawlers/spiders/tiggercomp.py
@@ -10,7 +10,7 @@ from websites_crawlers.utils import get_info
 class TiggerCompSpider(scrapy.Spider):
     name = "tiggercomp"
     allowed_domains = ["www.tiggercomp.com.br"]
-    start_urls = ["http://www.tiggercomp.com.br/novaloja2/"]
+    start_urls = ["https://tiggercomp.com.br/novaloja2/index.php?route=product/category&path=277"]
 
     custom_settings = {
         "CONCURRENT_REQUESTS": 64,
@@ -28,36 +28,32 @@ class TiggerCompSpider(scrapy.Spider):
             "Accept-Encoding": "gzip, deflate",
             "Accept-Language": "en-US,en;q=0.9",
             "Connection": "keep-alive",
+            "Cache-Control": "max-age=0",
             "Host": "www.tiggercomp.com.br",
-            "Referer": "http://www.tiggercomp.com.br/novaloja/product_info.php?products_id=6823",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
             "Upgrade-Insecure-Requests": "1",
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36",
         }
 
         categories_urls = [
-            cat for cat in response.xpath("//table[@class='infoBoxContents']/tr/td/a/@href").getall()
+            cat for cat in
+            response.xpath('//div[@class="row"]/aside/div/a/@href').getall()
+
         ]
 
         for i, url in enumerate(categories_urls):
+            yield scrapy.Request(
+                url,
+                method="GET",
+                headers=headers,
+                meta={"cookiejar": i},
+                callback=self.parse_products,
+                dont_filter=True,
+            )
 
-            if "cPath" in url:
-                yield scrapy.Request(
-                    url,
-                    method="GET",
-                    headers=headers,
-                    meta={"cookiejar": i},
-                    callback=self.parse_products,
-                    dont_filter=True,
-                )
-            elif "products_id" in url:
-                yield scrapy.Request(
-                    url,
-                    method="GET",
-                    headers=headers,
-                    meta={"cookiejar": i},
-                    callback=self.parse_each_product,
-                    dont_filter=True,
-                )
 
     def parse_products(self, response, **kwargs):
         """Function to get all products from a specific category."""
@@ -72,60 +68,13 @@ class TiggerCompSpider(scrapy.Spider):
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36",
         }
 
-        products_urls = [
-            prod for prod in response.xpath("//td[@class='productListing-data']/a/@href").getall()
-        ]
+        if 'Subcategoria' not in response.text:
+            products_urls = [
+                prod for prod in response.xpath('//div[@class="row"]/div/div/div/a/@href').getall()
+            ]
 
-        for url in products_urls:
-            yield scrapy.Request(
-                url,
-                method="GET",
-                headers=headers,
-                meta=response.meta,
-                callback=self.parse_each_product,
-                dont_filter=True,
-            )
-
-        if (
-            response.xpath("//td[@class='smallText']/a/u").getall() and
-            "PRÓXIMO" in response.xpath("//td[@class='smallText']/a/u").getall()[-1]
-        ):
-            # Checks if there are more pages to get products.
-
-            url = response.xpath("//td[@class='smallText']/a/@href").getall()[-1]
-
-            yield scrapy.Request(
-                url,
-                method="GET",
-                headers=headers,
-                meta={"cookiejar": 0},
-                callback=self.parse_products,
-                dont_filter=True,
-                cb_kwargs=kwargs,
-            )
-
-    def parse_each_product(self, response, **kwargs):
-        """Function to parse each product."""
-
-        if response.xpath("//td[@class='productListing-data']/a/@href").getall():
-            # Checks that we are on the product listing page (and not on a product page). If true, go to the product page.
-
-            product_id = response.xpath("//td[@class='productListing-data']/a/@href").getall()[0].split("=")[-1]
-
-            headers = {
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "Accept-Encoding": "gzip, deflate",
-                "Accept-Language": "en-US,en;q=0.9",
-                "Connection": "keep-alive",
-                "Host": "www.tiggercomp.com.br",
-                "Referer": f"http://www.tiggercomp.com.br/novaloja/product_info.php?products_id={product_id}",
-                "Upgrade-Insecure-Requests": "1",
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36",
-            }
-
-            url = response.xpath("//td[@class='productListing-data']/a/@href").getall()[0]
-
-            yield scrapy.Request(
+            for url in products_urls:
+                yield scrapy.Request(
                     url,
                     method="GET",
                     headers=headers,
@@ -134,59 +83,89 @@ class TiggerCompSpider(scrapy.Spider):
                     dont_filter=True,
                 )
 
+            if (
+                    'total de 1 ' not in response.xpath('//div[@class="col-sm-6 text-right"]')
+                    and response.xpath('//div[@class="col-sm-6 text-left"]/ul[@class="pagination"]')
+            ):
+
+                next_urls = [next_url for next_url in
+                 set(response.xpath('//div[@class="col-sm-6 text-left"]/ul[@class="pagination"]/li/a/@href').getall())]
+
+                for i, url in enumerate(next_urls):
+                    yield scrapy.Request(
+                        url,
+                        method="GET",
+                        headers=headers,
+                        meta={"cookiejar": i},
+                        callback=self.parse_products,
+                        dont_filter=True,
+                    )
+
         else:
-            # Product name
-            product_name = get_info(response, "property", "og:title")
+            sub_categories_urls = [
+                sub_cat for sub_cat in response.xpath('//div[@class="col-sm-3"]/ul/li/a/@href').getall()
+                if 'route=product/category&path' in sub_cat
+            ]
 
-            # Product description
-            product_description = get_info(response, "property", "og:description")
+            for i, url in enumerate(sub_categories_urls):
+                yield scrapy.Request(
+                    url,
+                    method="GET",
+                    headers=headers,
+                    meta={"cookiejar": i},
+                    callback=self.parse_products,
+                    dont_filter=True,
+                )
 
-            # Product id
-            product_id = response.url.split("=")[-1]
 
-            # Product image
-            product_image = get_info(response, "property", "og:image")
+    def parse_each_product(self, response, **kwargs):
+        """Function to parse each product."""
 
-            # Product currency
-            if [s for s in response.xpath("//table[@class='infobox']/tr/td[@align='center']/text()").getall() if "R$" in s]:
-                currency_symbol = re.findall("R*\$*", [s for s in response.xpath("//table[@class='infobox']/tr/td[@align='center']/text()").getall() if "R$" in s][0])[0]
-            elif response.xpath("//span[@class='productSpecialPrice']/text()").get():
-                currency_symbol = re.findall("R*\$*", response.xpath("//span[@class='productSpecialPrice']/text()").get())[0]
-            else:
-                currency_symbol = re.findall("R*\$*", [s for s in response.xpath("//td[@class='pageHeading']/text()").getall() if "R$" in s][0])[1]
-            
-            if currency_symbol == "R$":
-                currency_iso = "BRL"
-            elif currency_symbol == "$":
-                currency_iso = "USD"
+        # Product name
+        product_name = re.findall("(?<=title>)((.*?)+(?=</))", response.text)[0][0]
 
-            # Product in stock
-            if response.xpath("//img[@title=' Esgotado, avise-me ']"):
-                in_stock= False
-            else:
-                in_stock = True
+        # Product description
+        product_description = "" # not using this
 
-            # Product price
-            if response.xpath("//table[@class='infobox']/tr/td[@align='center']/text()").getall():
-                product_price = re.findall("\d+,\d+", [s for s in response.xpath("//table[@class='infobox']/tr/td[@align='center']/text()").getall() if "R$" in s][0])
-            elif response.xpath("//span[@class='productSpecialPrice']/text()").get():
-                product_price = re.findall("\d+,\d+", response.xpath("//span[@class='productSpecialPrice']/text()").get())[0]
-            else:
-                product_price = re.findall("\d+,\d+", [s for s in response.xpath("//td[@class='pageHeading']/text()").getall() if "R$" in s][0])[0]
+        # Product id
+        product_id = response.url.split("=")[-1]
 
-            if product_name:
-                yield {
-                    "product_name": product_name.lower(),
-                    "product_description": product_description,
-                    "product_labels": None,
-                    "product_id": product_id,
-                    "product_price": product_price,
-                    "product_image": product_image,
-                    "product_url": response.url,
-                    "currency_iso": currency_iso,
-                    "currency_symbol": currency_symbol,
-                    "in_stock": in_stock,
-                    "execution_date": str(date.today().strftime("%Y/%m/%d")),
-                    "website_domain": "tiggercomp",
-                    "website_url": "http://www.tiggercomp.com.br/novaloja/",
-                }
+        # Product image
+        if response.xpath('//div[@class="col-sm-8"]/ul[@class="thumbnails"]'):
+            product_image = response.xpath('//div[@class="col-sm-8"]/ul[@class="thumbnails"]/li/a/@href').get()
+        else:
+            product_image = None
+
+        # Product currency
+        currency_symbol = response.xpath('//div[@class="col-sm-4"]/ul[@class="list-unstyled"]/li/h2/text()').getall()[0][0:2]
+
+        if currency_symbol == "R$":
+            currency_iso = "BRL"
+        elif currency_symbol == "$":
+            currency_iso = "USD"
+
+        # Product in stock
+        if "Em estoque" in response.xpath('//div[@class="col-sm-4"]/ul[@class="list-unstyled"]/li/text()').getall()[1]:
+            in_stock= True
+        else:
+            in_stock = False
+
+        # Product price
+        product_price = re.findall('\d*\.*\d+,*\d*', response.xpath('//div[@class="col-sm-4"]/ul[@class="list-unstyled"]/li/h2/text()').getall()[0])[0]
+
+        if product_name:
+            yield {
+                "product_name": product_name.lower(),
+                "product_description": product_description,
+                "product_labels": None,
+                "product_id": product_id,
+                "product_price": product_price,
+                "product_image": product_image,
+                "product_url": response.url,
+                "currency_iso": currency_iso,
+                "currency_symbol": currency_symbol,
+                "in_stock": in_stock,
+                "execution_date": str(date.today().strftime("%Y/%m/%d")),
+                "website_domain": "tiggercomp",
+                "website_url": "http://www.tiggercomp.com.br/novaloja/",
+            }
